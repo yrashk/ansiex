100e5ec8a23ebc62a683aa7bf77282dfcf515ac0
Yurii Rashkovskii <yrashk@gmail.com>
2012-10-28 15:17:43 -0700

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "ansiex", description: "ANSI escape sequences",
+                 version: :head, keywords: ["ansi","escape","code","terminal","color","font"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/ansiex"]])
+    
